f3e92492dc9ad3cd8dcfeefa2a0f11f8d04f2c4a
Jeremiah Abraham <appen.jeremiah@gmail.com>
2026-03-04 13:38:20 +0100

    views

--- app.js
+++ app.js
@@ -655,7 +655,7 @@ function createFeedPost(p) {
         <div><p class="viewe">View all ${commentCount || 0} discuss</p></div>
       </div>
       <div class="twits">
-        <div><img class="lefti" src="pics/stats.svg"></div>
+        <div><svg xmlns="http://www.w3.org/2000/svg" class="lefti" viewBox="0 0 24 24" width="18" height="18" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"/><circle cx="12" cy="12" r="3"/></svg></div>
         <div><p class="viewe">${p.views || 0} views</p></div>
       </div>
     </div>
